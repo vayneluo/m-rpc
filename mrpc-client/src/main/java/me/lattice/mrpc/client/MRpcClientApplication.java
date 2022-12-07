4907df65f553919a43a6fbdc2b502b384138cd2f
vayneluo <vayneluo@haizol.com>
2022-12-07 17:15:50 +0800

    feat: mrpc client build

---
+++ mrpc-client/src/main/java/me/lattice/mrpc/client/MRpcClientApplication.java
@@ -0,0 +1,13 @@
+package me.lattice.mrpc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MRpcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MRpcClientApplication.class, args);
+	}
+
+}
